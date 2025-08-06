303dbfca1ec557c9f05ef7d10045591e445c5401
Alsanoce <40732771+Alsanoce@users.noreply.github.com>
2025-08-06 10:27:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['uuid'], // أضف هذا السطر
+    },
+  },
+  optimizeDeps: {
+    include: ['uuid'], // وأضف هذا السطر أيضًا
+  }
 })
